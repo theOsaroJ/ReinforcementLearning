e9b46b1e3f9e17376bc570fc621ef4142e840257
Osaro, 'Nosa James <64130121+theOsaroJ@users.noreply.github.com>
2024-01-04 17:52:49 -0500

    Create run.sh

---
+++ Qlearning/MOFs/Pressure_Adsorption/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#$ -q hpc@@colon
+#$ -N RL
+#$ -pe smp 16
+
+module load python
+
+python3 RL.py > output_RL.txt
+
+if [[ ! -d Results ]]; then
+mkdir Results
+fi
+
+mv Prior_data* Results
+mv Predicted* Results
