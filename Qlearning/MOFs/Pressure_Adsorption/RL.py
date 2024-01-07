02b4b9b95d3ec1c35a820bb22d10aedf827fd678
Osaro, 'Nosa James <64130121+theOsaroJ@users.noreply.github.com>
2024-01-07 18:12:58 -0500

    Update RL.py

--- Qlearning/MOFs/Pressure_Adsorption/RL.py
+++ Qlearning/MOFs/Pressure_Adsorption/RL.py
@@ -5,7 +5,7 @@ Authors: Etinosa Osaro, Yamil J Colon
 import numpy as np
 import pandas as pd
 from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.gaussian_process.kernels import RationalQuadratic
+from sklearn.gaussian_process.kernels import RationalQuadratic, Matern
 from sklearn.metrics import r2_score
 from sklearn.model_selection import ParameterGrid
 import os
@@ -127,10 +127,10 @@ def train_q_learning(env, gp, alpha, gamma, epsilon, max_episodes):
 
 param_grid = {
     'alpha': [0.001, 0.01, 0.1],
-    'gamma': [0.1, 0.5, 0.9],
-    'epsilon': [0.1, 0.5, 0.9],
-    'max_episodes': [5, 10],
-    'kernel':[RationalQuadratic(length_scale=50, alpha=0.5,length_scale_bounds=(1e-8,1e8),alpha_bounds=(1e-8,1e8))]
+    'gamma': [0.1, 0.3, 0.5, 0.7, 0.9],
+    'epsilon': [0.1, 0.3, 0.5, 0.7, 0.9],
+    'max_episodes': [5, 10, 15],
+    'kernel':[RationalQuadratic(length_scale=50, alpha=0.5,length_scale_bounds=(1e-8,1e8),alpha_bounds=(1e-8,1e8)), Matern(length_scale=50, nu=1.5)]
 }
 
 # Function to save data to a file with specific parameters
